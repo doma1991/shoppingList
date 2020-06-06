b6d6644dc8ad97685d8f7333571e17dd2bc4f61c
Dominika <do_ma1991@hotmail.com>
2020-06-06 18:10:13 +0100

    Adding a clickable check button which changes colour from white to green

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const ListItem = ({item, deleteItem}) => {
+
+const [buttonColour, updateButtonColour] = useState('white');
+
   return (
 <TouchableOpacity style={styles.listItem}>
 <View style={styles.listItemView}>
 <Text style={styles.listItemText}>{item.text}</Text>
-<Icon name="remove" size={20} color="firebrick" onPress={() => deleteItem(item.id)}/>
+<Icon name="check-square" size={20} 
+style={{flex: 3, color: buttonColour}}
+onPress={() => buttonColour == "white" ? updateButtonColour("green") : updateButtonColour("white")} 
+/>
+<Icon name="remove" size={20} color="firebrick" style={styles.removeView} onPress={() => deleteItem(item.id)}/>
 </View>
 </TouchableOpacity>
   );
@@ -25,8 +32,12 @@ listItemView: {
     justifyContent: 'space-between',
     alignItems: 'center'
 },
-listItemTest: {
-    fontSize: 18
+listItemText: {
+    fontSize: 18,
+    flex: 10
+},
+removeView: {
+    flex: 1,
 }
 });
 
