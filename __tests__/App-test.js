d3add6666919d0d039ab0c3c470756de02e2e7af
Dominika <do_ma1991@hotmail.com>
2020-07-11 14:55:26 +0100

    Adding test to App-tests to check that the header renders correctly

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,16 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import Header from '../components/Header';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('Renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('Renders Header component correctly', () => {
+  const header = renderer.create(<Header />).toJSON();
+  expect(header).toMatchSnapshot();
 });
