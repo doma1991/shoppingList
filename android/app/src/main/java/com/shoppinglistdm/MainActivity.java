9eb4efd1ef3484dc8df10025ee81f56f272fe614
Dominika <do_ma1991@hotmail.com>
2020-09-13 16:25:30 +0100

    Prepping for release

--- android/app/src/main/java/com/shoppinglistdm/MainActivity.java
+++ android/app/src/main/java/com/shoppinglistdm/MainActivity.java
@@ -1,4 +1,4 @@
-package com.shoppinglist;
+package com.shoppinglistdm;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/shoppinglistdm/MainApplication.java
+++ android/app/src/main/java/com/shoppinglistdm/MainApplication.java
@@ -1,4 +1,4 @@
-package com.shoppinglist;
+package com.shoppinglistdm;
 
 import android.app.Application;
 import android.content.Context;
