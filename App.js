af7778ac7d688d1eb1caf9ed7e7f0b887f6861a6
Dominika <do_ma1991@hotmail.com>
2020-09-20 17:05:45 +0100

    Adding functionality to save data to local storage

--- App.js
+++ App.js
@@ -1,45 +1,74 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import AddItem from './components/AddItem';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const App = () => {
-const [items, setItems] = useState([
-  {id: uuidv4(), text: 'Milk'},
-  {id: uuidv4(), text: 'Eggs'},
-  {id: uuidv4(), text: 'Bread'},
-  {id: uuidv4(), text: 'Butter'},
-]);
 
-const deleteItem = (id) => {
-  setItems(prevItems => {
-    return prevItems.filter(item => item.id != id);
-  });
-}
+  const [items, setItems] = useState([]);
 
-const addItem = (text) => {
-  if(!text) {
-Alert.alert('Error', 'Please enter an item', [{text: 'OK'}]);
-  } else {
-setItems(prevItems => { return [{id: uuidv4(), text}, ...prevItems]; });
-//clear textbox 
-}
-}
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem("KEY", JSON.stringify(items))
+    } catch (e) {
+      Alert.alert('Failed to save the data to the storage.')
+    }
+  }
+
+  const readData = async () => {
+    try {
+      const userData = await AsyncStorage.getItem("KEY")
+      setItems(JSON.parse(userData));
+    } catch (e) {
+      Alert.alert('Failed to fetch the data from storage.')
+    }
+  }
+
+  useEffect(() => {
+    readData()
+  }, [])
+
+  const removeData = async () => {
+    try {
+      await AsyncStorage.removeItem("KEY");
+    } catch (error) {
+      Alert.alert('Failed to delete data from storage.')
+    }
+  }
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+    removeData();
+    saveData();
+  }
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item.', [{ text: 'OK' }]);
+    } else {
+      setItems(prevItems => { return [{ id: uuidv4(), text }, ...prevItems]; })
+      removeData();
+      saveData();
+    }
+  }
 
   return (
-<View style={styles.container}>
-<Header title='Shopping List' />
-<AddItem addItem={addItem} />
-<FlatList data={items} renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>}/>
-</View>
+    <View style={styles.container}>
+      <Header title='Shopping List' />
+      <AddItem addItem={addItem} />
+      <FlatList data={items} renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem} />} />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     paddingTop: 60,
   },
 })
